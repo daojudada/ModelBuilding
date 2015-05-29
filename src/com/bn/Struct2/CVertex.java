35055309f4bdebe9e7374091d10a2098a1d80a52
CGH123 <327048688@qq.com>
2015-05-29 21:17:03 +0800

    Revert "Revert "fitst commit""
    This reverts commit 342c4b6ed412860a30560523282fe03805bd7d02.

---
+++ src/com/bn/Struct2/CVertex.java
@@ -0,0 +1,30 @@
+package com.bn.Struct2;
+
+
+
+
+
+
+
+public class CVertex {
+	private static int id = 0;
+	
+	public int		vertex_id;
+	public Point3f	vcoord;
+	public CVertex	next;
+	public CVertex	prev;
+	public CVertex()
+	{
+		vertex_id = id ++;
+		vcoord=new Point3f(0,0,0);
+		next = null;
+		prev = null;
+	}
+
+
+	public void SetCoord(Point3f point)
+	{
+		vcoord=point;
+	}
+}
+
---
+++ src/com/bn/drawop/OperationManage.java
@@ -0,0 +1,206 @@
+package com.bn.drawop;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
+import com.bn.drawop.Operation.Op;
+
+
+
+/**
+ * 操作管理�?
+ * @author GuoJun
+ *
+ */
+public class OperationManage {
+	
+	/**
+	 * operation list
+	 */
+	private LinkedList<Operation> listDraw;
+	/**
+	 * operation stack
+	 */
+	private Stack<Operation> stOperation;
+	/**
+	 * undo operation recycle stack
+	 */
+	private Stack<Operation> stRecycle;
+	
+	/**
+	 * now draw stack
+	 */
+	private Stack<OpDraw> stNowDraw;
+
+	
+	public enum DrawMode{RE,ADD,FILL};
+	/**
+	 * 绘图模式
+	 * RE：重绘画�?
+	 * ADD：增量绘�?
+	 * NOW：重绘当前路�?
+	 */
+	private DrawMode mode;
+	
+	public OperationManage() {
+		listDraw = new LinkedList<Operation>();
+		stOperation = new Stack<Operation>();
+		stRecycle = new Stack<Operation>();
+		stNowDraw = new Stack<OpDraw>();
+		mode = DrawMode.RE;
+	}
+	
+	/**
+	 * push in list
+	 * @param op
+	 */
+	public void pushOp(Operation op)
+	{
+		stOperation.push(op);
+	}
+	
+	/**
+	 * pop out list
+	 * @param op
+	 * @return stOperation.Last
+	 */
+	public Operation popOp()
+	{
+		Operation op = stOperation.pop();
+		return op;
+	}
+	
+	/**
+	 * push in listDraw
+	 * @param listDraw
+	 */
+	public void pushDraw(OpDraw op)
+	{
+		listDraw.add(op);
+		stNowDraw.push(op);
+	}
+	
+	/**
+	 * push in listDraw
+	 * @param listDraw
+	 */
+	public void pushNowDraw(OpDraw op)
+	{
+		stNowDraw.push(op);
+	}
+	
+	/**
+	 * push in listDraw
+	 * @param opFill
+	 */
+	public void pushFill(OpFill opFill)
+	{
+		listDraw.add(opFill);
+	}
+	
+	/**
+	 * pop out listDraw
+	 * @param op
+	 * @return stOperation.Last
+	 */
+	public Operation popDraw()
+	{
+		Operation op = listDraw.getLast();
+		listDraw.removeLast();
+		stNowDraw.pop();
+		return op;
+	}
+
+	public Operation popNowDraw()
+	{
+		return stNowDraw.pop();
+	}
+	
+	/**
+	 * pop out opFill
+	 * @param op
+	 * @return stOperation.Last
+	 */
+	public Operation popFill()
+	{
+		Operation op = listDraw.getLast();
+		listDraw.removeLast();
+		return op;
+	}
+
+	
+	
+	public OpDraw getNowDraw()
+	{
+		if(stNowDraw.isEmpty())
+			return null;
+		else
+			return stNowDraw.lastElement();
+	}
+	
+	public Operation getDrawLast()
+	{
+		if(listDraw.isEmpty())
+			return null;
+		else
+			return listDraw.getLast();
+	}
+	
+	
+	public Iterator<Operation> getDrawIterator()
+	{
+		return listDraw.iterator();
+	}
+	
+	/**
+	 * 绘图模式
+	 */
+	public void setMode(DrawMode m)
+	{
+		mode = m;
+	}
+	
+	public DrawMode getMode()
+	{
+		return mode;
+	}
+	
+	public void clear()
+	{
+		stOperation.clear();
+		stRecycle.clear();
+		stNowDraw.clear();
+		listDraw.clear();
+		
+	}
+	
+	public int size()
+	{
+		return stOperation.size();
+		
+	}
+	
+	public void redo()
+	{
+		if(!stRecycle.isEmpty())
+		{
+			Operation op = stRecycle.pop();
+			if(op.getType() == Op.TRANS)
+				((OpTrans)op).setIsRedo(true);
+			op.Redo();
+			pushOp(op);
+		}
+	}
+	
+	public void undo()
+	{
+		if(!stOperation.isEmpty())
+		{
+			Operation op = popOp();
+			op.Undo();
+			stRecycle.push(op);
+		}
+	}
+
+}
